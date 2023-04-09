1c1c324469a4161bb650d4d332b15ac513e6c5eb
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-04-09 12:55:52 +0200

    setup three.js frontend

---
+++ ui/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { init } from './canvas';
+
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    init(canvas);
+  });
+</script>
+
+<main>
+  <canvas bind:this={canvas} />
+</main>
